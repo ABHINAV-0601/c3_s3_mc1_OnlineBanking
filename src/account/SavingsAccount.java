73b3901011de76d8aea03242493a0ec8e8c4ab7c
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 17:26:05 +0530

    declared attributes

---
+++ src/account/SavingsAccount.java
@@ -0,0 +1,14 @@
+package account;
+
+public class SavingsAccount {
+    private String accountHolderName;
+    private long accountNumber;
+    private String accountOpeningDate;
+    private String accountStatus;
+    private double availableBalance;
+    private double totalBalance;
+    private String internetAndMobileBankingEnabled;
+    private double interestRate;
+    private String modeOfOperation;
+
+}
