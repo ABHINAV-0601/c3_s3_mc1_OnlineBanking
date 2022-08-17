1625a419b02fc3372aa7c4ecef1848a96bc63c34
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-17 08:51:21 +0530

    completed the task

--- src/account/SavingsAccountImpl.java
+++ src/account/SavingsAccountImpl.java
@@ -16,16 +16,17 @@ public class SavingsAccountImpl {
         System.out.println("Account Holder Name is :" + savingsAccount.getAccountHolderName());
         System.out.println("Account number is :" + savingsAccount.getAccountNumber());
         System.out.println("Account Opening Date is :"+ savingsAccount.getAccountOpeningDate());
-        System.out.println("Accont Status is :"+ savingsAccount.getAccountStatus());
+        System.out.println("Account Status is :"+ savingsAccount.getAccountStatus());
         System.out.println("Available balance in the account is : $"+ savingsAccount.getAvailableBalance());
         System.out.println("Total balance in the account is : $"+ savingsAccount.getTotalBalance());
         System.out.println("Is Internet and Mobile Banking Enabled :"+ savingsAccount.getInternetAndMobileBankingEnabled());
         System.out.println("Interest Rate is :" + savingsAccount.getInterestRate() +"%");
         System.out.println("Mode of Operation is :" + savingsAccount.getModeOfOperation());
 
-        System.out.println("Account Balance on retrieving is :"+savingsAccount.retrieveBalance());
+        System.out.println("Account Balance on retrieving is : $"+savingsAccount.retrieveBalance());
 
-        System.out.println("Account balance after credit cash is :"+savingsAccount.creditCash(500));
-        System.out.println("Account balance after debit cash is :"+savingsAccount.debitCash(300));
+        System.out.println("Account balance after credit cash is : $"+savingsAccount.creditCash(500));
+
+        System.out.println("Account balance after debit cash is : $"+savingsAccount.debitCash(300));
     }
 }
